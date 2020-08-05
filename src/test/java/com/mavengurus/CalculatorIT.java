8cb378c8513c4b78ff2c3dafc1cedea1d2242d6a
ASKRIGhassen <gh.askri@gmail.com>
2020-08-05 13:28:54 +0200

    failsafe & jacoco plugins

---
+++ src/test/java/com/mavengurus/CalculatorIT.java
@@ -0,0 +1,11 @@
+package com.mavengurus;
+
+import org.junit.jupiter.api.Test;
+
+public class CalculatorIT {
+
+    @Test
+public void myItTest(){
+    System.out.println("running my integration testing !");
+}
+}
---
+++ src/test/java/com/mavengurus/CalculatorTest.java
@@ -0,0 +1,35 @@
+package com.mavengurus;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void sum() {
+        assertEquals(Calculator.sum(2,3),5);
+    }
+
+    @Test
+    void sub() {
+        assertEquals(Calculator.sub(3,1),2);
+    }
+
+    @Test
+    void multiply() {
+        assertEquals(Calculator.multiply(3,2),6);
+    }
+
+    @Test
+    void divide() {
+        assertEquals(Calculator.divide(6,2),3);
+    }
+
+    @Test
+    void divideByZero(){
+        assertThrows(RuntimeException.class,() -> {
+            Calculator.divide(4,0);
+        });
+    }
+}
